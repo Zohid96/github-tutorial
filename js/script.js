8aed49e25e4f5b3451b798b76926da53b299aceb
Zohid <example@gmail.com>
2023-11-20 17:38:49 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+"use strict";
+alert("go go go")
+let number = 10;
+const borderColor = "red";
+
+number = 100;
+  
+console.log (number)
+
+const Zohid = {
+    FristName:"Zokirboyev",
+    age: 25,
+    islong: true,
+};
+console.log (Zohid)
+
+const colors = ["red", "black", 10, [1,3,"zzzz"],{age: 25, islong: true }];
+console.log(colors[1]);
